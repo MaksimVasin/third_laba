19804227f7a8e42200b4661fac566ff41dfe67b2
avaniar <avaniar3@gmail.com>
2021-12-02 19:01:46 +0300

    Demonstrating Shape Factory work and adding a unit tests.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,39 @@
 fun main() {
     val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
-    myShapeFactory.createCircle(10.0).calcArea()
-    myShapeFactory.createSquare(10.0).calcArea()
-    myShapeFactory.createRectangle(10.0, 20.0).calcPerimeter()
-    myShapeFactory.createTriangle(20.0, 30.0, 40.0).calcPerimeter()
-    myShapeFactory.createRandomShape().calcArea()
-    myShapeFactory.createRandomTriangle().calcPerimeter()
+    val listShapes: List<Shape> = listOf(
+        myShapeFactory.createCircle(10.0),
+        myShapeFactory.createSquare(10.0),
+        myShapeFactory.createRectangle(10.0, 20.0),
+        myShapeFactory.createTriangle(20.0, 30.0, 40.0)
+    )
+    println("User-defined shapes")
+    println("Sum area: ${listShapes.sumOf { it.calcArea() }}")
+    println("Sum perimeter: ${listShapes.sumOf { it.calcPerimeter() }}")
+    println("Shape with max area: ${listShapes.maxByOrNull { it.calcArea() }} / min area: ${listShapes.minByOrNull { it.calcArea() }}")
+    println("Shape with max perimeter: ${listShapes.maxByOrNull { it.calcArea() }} / min perimeter: ${listShapes.minByOrNull { it.calcArea() }}")
+    println()
+
+    val listShapesRandomParameters: List<Shape> = listOf(
+        myShapeFactory.createRandomCircle(),
+        myShapeFactory.createRandomSquare(),
+        myShapeFactory.createRandomRectangle(),
+        myShapeFactory.createRandomTriangle(),
+    )
+    println("Random parameters shapes")
+    println("Sum area: ${listShapesRandomParameters.sumOf { it.calcArea() }}")
+    println("Sum perimeter: ${listShapesRandomParameters.sumOf { it.calcPerimeter() }}")
+    println("Shape with max area: ${listShapesRandomParameters.maxByOrNull { it.calcArea() }} / min area: ${listShapesRandomParameters.minByOrNull { it.calcArea() }}")
+    println("Shape with max perimeter: ${listShapesRandomParameters.maxByOrNull { it.calcArea() }} / min perimeter: ${listShapesRandomParameters.minByOrNull { it.calcArea() }}")
+    println()
+
+    val listRandomShapes: List<Shape> = listOf(
+        myShapeFactory.createRandomShape(),
+        myShapeFactory.createRandomShape(),
+        myShapeFactory.createRandomShape(),
+    )
+    println("Random shapes")
+    println("Sum area: ${listRandomShapes.sumOf { it.calcArea() }}")
+    println("Sum perimeter: ${listRandomShapes.sumOf { it.calcPerimeter() }}")
+    println("Shape with max area: ${listRandomShapes.maxByOrNull { it.calcArea() }} / min area: ${listRandomShapes.minByOrNull { it.calcArea() }}")
+    println("Shape with max perimeter: ${listRandomShapes.maxByOrNull { it.calcArea() }} / min perimeter: ${listRandomShapes.minByOrNull { it.calcArea() }}")
 }
---
+++ src/test/kotlin/ShapeFactory_Test.kt
@@ -0,0 +1,124 @@
+import kotlin.test.assertEquals
+import kotlin.test.Test
+
+internal class ShapeFactory_Test {
+    ///////////////// Circle ////////////////////////////////////////////////////////////
+    @Test
+    fun createCircleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createCircle(10.0)
+    }
+    @Test(expected = IllegalArgumentException::class)
+    fun createCircleErrorTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createCircle(-10.0)
+    }
+    @Test
+    fun createRandomCircleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createRandomCircle()
+    }
+    @Test
+    fun areaCircleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        assertEquals(Math.PI * 10.0 * 10.0, myShapeFactory.createCircle(10.0).calcArea())
+    }
+    @Test
+    fun perimeterCircleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        assertEquals(2 * Math.PI * 10.0, myShapeFactory.createCircle(10.0).calcPerimeter())
+    }
+    ///////////////// Square ////////////////////////////////////////////////////////////
+    @Test
+    fun createSquareTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createSquare(10.0)
+    }
+    @Test(expected = IllegalArgumentException::class)
+    fun createSquareErrorTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createSquare(-10.0)
+    }
+    @Test
+    fun createRandomSquareTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createRandomSquare()
+    }
+    @Test
+    fun areaSquareTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        assertEquals(10.0 * 10.0, myShapeFactory.createSquare(10.0).calcArea())
+    }
+    @Test
+    fun perimeterSquareTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        assertEquals(10.0 + 10.0 + 10.0 + 10.0, myShapeFactory.createSquare(10.0).calcPerimeter())
+    }
+    ///////////////// Rectangle //////////////////
+    @Test
+    fun createRectangleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createRectangle(10.0, 10.0)
+    }
+    @Test(expected = IllegalArgumentException::class)
+    fun createRectangleWidthErrorTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createRectangle(-10.0, 10.0)
+    }
+    @Test(expected = IllegalArgumentException::class)
+    fun createRectangleHeightErrorTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createRectangle(10.0, -10.0)
+    }
+    @Test
+    fun createRandomRectangleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createRandomRectangle()
+    }
+    @Test
+    fun areaRectangleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        assertEquals(10.0 * 20.0, myShapeFactory.createRectangle(10.0, 20.0).calcArea())
+    }
+    @Test
+    fun perimeterRectangleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        assertEquals(10.0 + 10.0 + 20.0 + 20.0, myShapeFactory.createRectangle(10.0, 20.0).calcPerimeter())
+    }
+    ///////////////// Triangle ///////////////////
+    @Test
+    fun createTriangleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createCircle(10.0)
+    }
+    @Test(expected = IllegalArgumentException::class)
+    fun createTriangleSideAErrorTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createTriangle(-10.0, 10.0, 10.0)
+    }
+    @Test(expected = IllegalArgumentException::class)
+    fun createTriangleSideBErrorTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createTriangle(10.0, -10.0, 10.0)
+    }
+    @Test(expected = IllegalArgumentException::class)
+    fun createTriangleSideCErrorTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createTriangle(10.0, 10.0, -10.0)
+    }
+    @Test
+    fun createRandomTriangleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        myShapeFactory.createRandomTriangle()
+    }
+    @Test
+    fun areaTriangleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        assertEquals(30.0 * 40.0 / 2, myShapeFactory.createTriangle(30.0, 40.0, 50.0).calcArea())
+    }
+    @Test
+    fun perimeterTriangleTest() {
+        val myShapeFactory: ShapeFactory = ShapeFactoryImpl()
+        assertEquals(30.0 + 40.0 + 50.0, myShapeFactory.createTriangle(30.0, 40.0, 50.0).calcPerimeter())
+    }
+}
