6b48367183e04ebff5294485a02f22bf21545f9f
MaksimVasin <maksim.vasin.2001@mail.ru>
2021-12-11 20:50:00 +0300

    Revert "changing the privacy of fields"
    This reverts commit 5f41942beccb352e10b9163d759d918f6b364be1.

--- src/main/kotlin/Shapes.kt
+++ src/main/kotlin/Shapes.kt
@@ -7,37 +7,44 @@ interface Shape {
 }
 
 // creating a circle with a given radius
-class Circle(val radius: Double) : Shape {
+class Circle(private val radius: Double) : Shape {
     init {
         if (radius < 0)
             throw IllegalArgumentException("radius must be greater than zero")
     }
+
+    fun radiusGet(): Double = radius
     override fun calcArea(): Double = Math.PI * this.radius * this.radius
     override fun calcPerimeter(): Double = 2 * Math.PI * this.radius
 }
 
 // creating a square with the length of the side
-class Square(val side: Double) : Shape {
+class Square(private val side: Double) : Shape {
     init {
         if (side < 0)
             throw IllegalArgumentException("side must be greater than zero")
     }
+
+    fun sideGet(): Double = side
     override fun calcArea(): Double = side.pow(2.0)
     override fun calcPerimeter(): Double = side * 4
 }
 
 // creating a rectangle with long sides
-class Rectangle(val width: Double, val height: Double) : Shape {
+class Rectangle(private val width: Double, private val height: Double) : Shape {
     init {
         if ((width < 0) || (height < 0))
             throw IllegalArgumentException("side must be greater than zero")
     }
+
+    fun widthGet(): Double = width
+    fun heightGet(): Double = height
     override fun calcArea(): Double = width * height
     override fun calcPerimeter(): Double = (width + height) * 2
 }
 
 // creating a triangle with long sides
-class Triangle(val sideA: Double, val sideB: Double, val sideC: Double) : Shape {
+class Triangle(private val sideA: Double, private val sideB: Double, private val sideC: Double) : Shape {
     // half perimeter for calculating the area of a triangle
     private val halfPerimeter: Double = calcPerimeter() / 2
 
@@ -48,6 +55,10 @@ class Triangle(val sideA: Double, val sideB: Double, val sideC: Double) : Shape 
         if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
             throw IllegalArgumentException("impossible to build a triangle")
     }
+
+    fun sideAGet(): Double = sideA
+    fun sideBGet(): Double = sideB
+    fun sideCGet(): Double = sideC
     override fun calcArea(): Double =
         sqrt(halfPerimeter * (halfPerimeter - sideA) * (halfPerimeter - sideB) * (halfPerimeter - sideC))
 
